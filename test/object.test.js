2dd322dcd8ae5b60222939136df3f88419d7c15c
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-09-04 15:59:20 +0200

    Added object json tests

--- test/object.test.js
+++ test/object.test.js
@@ -1,11 +1,14 @@
 require('../dist/powerup.js');
 
-describe('json()', () =>
+describe('object.json()', () =>
 {
     test('list', () => {
         expect([1, 2, 3].json()).toBe('[1,2,3]')
     })
     test('dict', () => {
         expect({a:1,b:true}.json()).toBe('{"a":1,"b":true}')
+    })
+    test('number', () => {
+        expect(3.59.json()).toBe('3.59')
     })
 })

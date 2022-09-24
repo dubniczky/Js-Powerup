cfdf247f29184d63fe10fcafa88d0081b5317660
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-09-24 12:37:51 +0200

    Added various mathematical functions to Number

--- prototypes/number.js
+++ prototypes/number.js
@@ -15,3 +15,54 @@ Number.prototype.floor = function() {
 Number.prototype.pow = function(exponent, modulo = Number.MAX_VALUE) {
     return this ** exponent % modulo
 }
+
+/**
+ * Clamps the number between the given min and max values (inclusive)
+ *
+ * @returns {Number} The clamped number
+ */
+Number.prototype.clamp = function(min, max) {
+    return Math.min(Math.max(this, min), max)
+}
+
+/**
+ * Returns if the number is between the given min and max values (inclusive)
+ */
+Number.prototype.inRange = function(min, max) {
+    return this >= min && this <= max
+}
+
+/**
+ * A short-hand for Math.sqrt
+ */
+Number.prototype.sqrt = function() {
+    return Math.sqrt(this)
+}
+
+/**
+ * A short-hand for Math.abs
+ */
+Number.prototype.abs = function() {
+    return Math.abs(this)
+}
+
+/**
+ * Check if a number has no fractional part
+ */
+Number.prototype.isInt = function() {
+    return this % 1 === 0
+}
+
+/**
+ * Check if a number has a fractional part
+ */
+Number.prototype.isFloat = function() {
+    return this % 1 !== 0
+}
+
+/**
+ * Check if a number is in a specified range from a target value (inclusive)
+ */
+Number.prototype.isClose = function(target, range) {
+    return Math.abs(this - target) <= range
+}
--- test/number.test.js
+++ test/number.test.js
@@ -41,3 +41,88 @@ describe('Number.pow()', () =>
         expect(a.pow(4, -256)).toStrictEqual(113)
     })
 })
+
+describe('Number.clamp()', () =>
+{
+    test('inside', () => {
+        expect((5).clamp(1, 10)).toStrictEqual(5)
+    })
+
+    test('over', () => {
+        expect((-3).clamp(-100, -20)).toStrictEqual(-20)
+    })
+
+    test('under', () => {
+        expect((50).clamp(55, 56)).toStrictEqual(55)
+    })
+})
+
+describe('Number.inRange()', () =>
+{
+    test('inside', () => {
+        expect((5).inRange(1, 10)).toStrictEqual(true)
+    })
+
+    test('over', () => {
+        expect((-3).inRange(-100, -20)).toStrictEqual(false)
+    })
+
+    test('under', () => {
+        expect((50).inRange(55, 56)).toStrictEqual(false)
+    })
+})
+
+describe('Number.sqrt()', () =>
+{
+    test('correct', () => {
+        expect((9).sqrt()).toStrictEqual(3)
+    })
+})
+
+describe('Number.abs()', () =>
+{
+    test('positive', () => {
+        expect((9).abs()).toStrictEqual(9)
+    })
+
+    test('negative', () => {
+        expect((-12).abs()).toStrictEqual(12)
+    })
+
+    test('zero', () => {
+        expect((0).abs()).toStrictEqual(0)
+    })
+})
+
+describe('Number.isInt()', () =>
+{
+    test('int', () => {
+        expect((9).isInt()).toStrictEqual(true)
+    })
+
+    test('float', () => {
+        expect((12.9).isInt()).toStrictEqual(false)
+    })
+})
+
+describe('Number.isFloat()', () =>
+{
+    test('int', () => {
+        expect((9).isFloat()).toStrictEqual(false)
+    })
+
+    test('float', () => {
+        expect((12.9).isFloat()).toStrictEqual(true)
+    })
+})
+
+describe('Number.isClose()', () =>
+{
+    test('close', () => {
+        expect((9).isClose(10, 2)).toStrictEqual(true)
+    })
+
+    test('far', () => {
+        expect((0).isClose(-4, 2)).toStrictEqual(false)
+    })
+})
